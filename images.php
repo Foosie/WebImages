c059bf8c45b7fd2df1406b139abf5b37005d75cb
Don Mankin <don@desertsagesolutions.com>
2021-07-04 06:28:52 -0700

    Update images.php

--- images.php
+++ images.php
@@ -3,7 +3,7 @@
 /*
  * images37.php
  *
- * Copyright (c) 2021 Don Mankin (Foose) <don.mankin@yahoo.com>
+ * Copyright (c) 2021 Don Mankin (Foose, Fooser, Foosie)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
